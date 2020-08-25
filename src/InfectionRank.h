33b1f1bd31389c7d0d64fadfc96a4b9a2e61f5c7
leonkosarev <70164506+leonkosarev@users.noreply.github.com>
2020-08-25 20:41:25 +0300

    add comments

--- src/InfectionRank.h
+++ src/InfectionRank.h
@@ -4,13 +4,15 @@
 #include <list>
 #include <iostream>
 
+// Class for representing all patients with the same infection count.
+// Contains a list with the ID numbers of the relevant patients.
+
 class InfectionRank {
     unsigned int rank;
     std::list<unsigned int > patientList;
 
 public:
     explicit InfectionRank(unsigned int infectionRank);
-
 
     std::list<unsigned int>::iterator addPatient(unsigned int patientID);
 
@@ -20,7 +22,7 @@ public:
 
     void removePatient(std::list<unsigned int>::iterator patient);
 
-    void getTopK(int k, int * count , unsigned int* idArray);
+    void getTopK(int k, int * count , unsigned int* idArray); // Used to get the K top spreaders.
 
 };
 
